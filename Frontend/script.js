5a9f6913c910f18f426a58738b74ff935cfeb365
bahast47 <bahast@accountflow.no>
2022-10-10 07:05:36 +0200

    Displayed show names

--- Frontend/script.js
+++ Frontend/script.js
@@ -8,10 +8,27 @@ async function getApi() {
     let data = await response.json();
     if (response) {
         hideLoader();
+        show(data);
     }
+
 }
 
 getApi(api_url)
     .catch((error) => console.error("FETCH ERROR:", error));
 
 
+
+function show(data) {
+    const nameText = document.getElementsByClassName("nameText")[0];
+    console.log(nameText);
+    data.forEach((dataS) => {
+
+        let itemP = document.createElement("H1");
+        let itemPText = document.createTextNode(dataS.name);
+        itemP.appendChild(itemPText);
+        nameText.appendChild(itemP);
+        console.log(itemP);
+        console.log(dataS.name)
+    })
+
+}
